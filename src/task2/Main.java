19847a8adbc14a12aa01335a66e28c8d4004ac9a
Vasyl_Hulevych <vasiliy.mysiuk@gmail.com>
2019-04-11 00:02:11 +0300

    Changed formatting to two decimals

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -3,8 +3,10 @@ package task2;
 public class Main {
     public static void main(String[] args) {
         Accounting acc = new Accounting();
-        double result = acc.pay(1.5, 10);
-        double roundOff = Math.round(result*100)/100.00;
-        System.out.println(roundOff);
+        double result = acc.pay(1.5, 9.99);
+        System.out.println(String.format("%.2f", result));
+
+        //double roundOff = Math.round(result*100)/100.00;
+        //System.out.println(roundOff);
     }
 }
